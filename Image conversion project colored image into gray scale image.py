1c9bda6e6b5b38513486c596ae029815e92d9217
Kirti Mandloi <159609273+kirtimandloi@users.noreply.github.com>
2024-05-23 14:05:28 +0530

    Update Image conversion project colored image into gray scale image.py

--- Image conversion project colored image into gray scale image.py
+++ Image conversion project colored image into gray scale image.py
@@ -7,7 +7,6 @@ print("You Enter this===",path)
 #Now read image 
 img1 = cv2.imread(path,0) #convert image into grayscale
 img1 = cv2.resize(img1,(560,700))
-img1 = cv2.flip(img1,0)#it accept 3 parameters 0,-1,1
 cv2.imshow("converted image==",img1)
 k = cv2.waitKey(0)
 if k == ord("q"):
