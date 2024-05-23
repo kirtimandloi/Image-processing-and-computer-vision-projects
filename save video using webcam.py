828ef1ebaa3d56aa36456026e10ac5cee12d5d38
Kirti Mandloi <159609273+kirtimandloi@users.noreply.github.com>
2024-05-23 15:26:11 +0530

    Update save video using webcam.py

--- save video using webcam.py
+++ save video using webcam.py
@@ -4,13 +4,13 @@ cap = cv2.VideoCapture(0)#external cam 1 and laptop 0
 print(cap)
 
 fourcc = cv2.VideoWriter_fourcc(*"XVID") 
-output = cv2.VideoWriter("C:\\Users\\LENOVO\\Desktop\\Image processing and Computer Vision\\output.avi",fourcc,20.0,(640,480))
+output = cv2.VideoWriter("path of folder with name in which you want to save.avi",fourcc,20.0,(640,480))
 
 while cap.isOpened():
     ret,frame = cap.read()#give two values one is bool and second is frame
     if ret == True:
         cv2.imshow("frame", frame)
-        #frame = cv2.flip(frame,0)
+        #frame = cv2.flip(frame,0) -to flip the video 
         output.write(frame)
         if cv2.waitKey(1) & 0xFF == ord("x"):
             break
