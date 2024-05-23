61a9b7e71b29ee02989cc29af85257c8dfda7102
Kirti Mandloi <159609273+kirtimandloi@users.noreply.github.com>
2024-05-23 16:29:13 +0530

    Add files via upload

---
+++ Screen Recording System.py
@@ -0,0 +1,31 @@
+import cv2 as c
+import pyautogui as p 
+import numpy as np
+
+#create resolution
+rs = p.size()
+
+#filename in which we store recording
+fn = input("Please Enter any file name and Path")
+#Fix the frame rate
+fps = 60.0
+fourcc = c.VideoWriter_fourcc(*'XVID')
+output = c.VideoWriter(fn,fourcc,fps,rs)
+
+#create recording module
+c.namedWindow("Live_Recording",c.WINDOW_NORMAL)
+#Resize the window
+c.resizeWindow("Live_Recording",(600,400))
+
+while True:
+    img = p.screenshot()
+    f = np.array(img)
+    f = c.cvtColor(f,c.COLOR_BGR2RGB)
+    output.write(f)
+    c.imshow("Live_Recording", f)
+    if c.waitKey(1)==ord("x"):
+        break
+    
+output.release()
+c.destroyAllWindows()
+
