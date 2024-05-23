14f831517a22d572305b3ee793e6f05e490251bc
Kirti Mandloi <159609273+kirtimandloi@users.noreply.github.com>
2024-05-23 14:03:38 +0530

    Add files via upload

---
+++ Image conversion project colored image into gray scale image.py
@@ -0,0 +1,18 @@
+import cv2
+
+#Image conversion project colored image into gray scale image
+path = input("Enter the Path and name of an image===")
+print("You Enter this===",path)
+
+#Now read image 
+img1 = cv2.imread(path,0) #convert image into grayscale
+img1 = cv2.resize(img1,(560,700))
+img1 = cv2.flip(img1,0)#it accept 3 parameters 0,-1,1
+cv2.imshow("converted image==",img1)
+k = cv2.waitKey(0)
+if k == ord("q"):
+    cv2.destroyAllWindows()
+    
+elif k == ord("s"):
+    cv2.imwrite("C:\\Users\\LENOVO\\Desktop\\Image processing and Computer Vision\\output.png",img1)  #it accept name of image and data
+    cv2.destroyAllWindows()
